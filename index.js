ac5045cf3d0801796a972ebea70bf5e3358f145d
joedip16 <joedippolito16@gmail.com>
2020-11-23 18:55:44 -0500

    created questions thadquestions that fill in the readme file

---
+++ index.js
@@ -0,0 +1,78 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+//ask user for their input
+const promptUser = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the title of your project?",
+            name: "title"
+        },
+            {type: "input",
+             message: "What is the project description, intallation instructions, usage information, contribution guildlines, and test instructions?",
+             name: "description",
+        },
+            {type: "list",
+            message: "What is the license?",
+            choices: [
+                "MIT",
+                "Apache"
+                ],
+            name: "license"
+        },
+            {type: "input",
+            message: "What is your Github username?",
+            name: "gitHubName"
+            
+            },
+            {type: "input",
+            message: "What is your Email?",
+            name: "email"
+            
+            },
+            {type: "checkbox",
+            message: "How can you be reached?",
+            choices: [
+                "Cell Phone",
+                "Twitter",
+                "Slack"
+            ],
+            name: "contact"
+            },
+            {type: "list",
+            message: "Table of Contents",
+            choices:[
+                "Title", "Description, Installation, Usage, Contributing, and Tests", "License", "gitHubName", "Email", "Contact"
+            ],
+            name: "tableOfContents"
+            
+            },
+    ]).then (function(response){
+        console.log(response);
+        let createReadme = generateREADME(response);
+        console.log(createReadme);
+        writeFileAsync("README.md", createReadme).then(
+            err()
+        )
+    })
+}
+
+promptUser();
+
+function generateREADME(response){
+    let readMe =`#${response.title}
+    ######${response.description}
+    ######${response.license}
+    ######${response.gitHubName}
+    ######${response.email}
+    ######${response.contact}
+    ######${response.tableOfContents}`
+    return(readMe)
+}
+
+//using input generate html file to string format
